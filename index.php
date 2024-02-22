6a5224b84af969baa0d0494ca3c23b22a1f4c494
AhmedRazaDev <maraza008@icloud.com>
2024-02-23 04:49:58 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'head.php'; ?>
+</head>
+
+<body>
+
+    <section class="banner">
+        <?php include 'header.php'; ?>
+        <div class="container">
+            <div class="banner-txt">
+                <h1 class="text-white w-50 ">Protect Your Data with Opal Lock
+                    By Fidelity Height</h1>
+                <div class="dis-banner">
+                    <p class="txt-p w-50 text-white">Opal Lock is a desktop application that utilizes Opal SED
+                        technology
+                        to put the
+                        capabilities of
+                        hardware-based encryption in your own hands.</p>
+                    <p class="txt-p w-50 text-white">Designed to configure and manage built-in hardware-based
+                        encryption on
+                        compatible
+                        drives, Opal Lock
+                        provides data protection that cannot be cracked, even if your drive is physically lost or
+                        stolen.
+                    </p>
+                    <button type="button" class="btn btn-primary btn-log">Download</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec-2">
+        <div class="container">
+            <div class="inner-2">
+                <h1 class="h-1 text-white">OPAL LOCK PRODUCTS</h1>
+                <p class="text-sec2 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                    quam neque
+                    quibusdam corrupti
+                    aspernatur corporis alias nisi dolorum expedita veritatis voluptates minima sapiente.</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK USB</h3>
+                        <p class="clmn-txt1 text-white">The USB version supports all basic functionalities of Opal Lock
+                            for up to
+                            five USB-mounted
+                            Opal Drives. On top of features supported by the Demo version, the USB version.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK STANDARD</h3>
+                        <p class="clmn-txt1 text-white">The Standard version supports all basic functionalities of Opal
+                            Lock for up to five USB-mounted Opal Drives. On top of features supported by the Demo
+                            version, the USB version.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK PREMIUM</h3>
+                        <p class="clmn-txt1 text-white">The Standard version supports all basic functionalities of Opal
+                            Lock for up to five USB-mounted Opal Drives. On top of features supported by the Demo
+                            version, the USB version.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="sec-3">
+        <div class="container">
+            <div class="align-items-center row">
+                <div class="col-md-6">
+                    <div>
+                        <img src="images/Group 2055 (1).png">
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="inner-sec3">
+                        <h1 class="text-white">ABOUT US</h1>
+                        <p class="text-white">Fidelity Height specializes in software management of TCG self-encrypting
+                            drives. We are
+                            passionate about engineering streamlined solutions for our customers and providing them with
+                            tools to fully utilize their drives built-in hardware-based encryption.</p>
+                        <p class="text-white">Fidelity Height specializes in software management of TCG self-encrypting
+                            drives. We are
+                            passionate about engineering streamlined solutions for our customers and providing them with
+                            tools to fully utilize their drives’ built-in hardware-based encryption.</p>
+                        <button type="button" class="btn btn-primary btn-log">Download</button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="sec-2">
+        <div class="container">
+            <div class="inner-2">
+                <h1 class="h-1 text-white">Secure. Certified. Verified</h1>
+                <p class="text-sec2 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                    quam neque
+                    quibusdam corrupti
+                    aspernatur corporis alias nisi dolorum expedita veritatis voluptates minima sapiente.</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK USB</h3>
+                        <p class="clmn-txt1 text-white">The USB version supports all basic functionalities of Opal Lock
+                            for up to
+                            five USB-mounted
+                            Opal Drives. On top of features supported by the Demo version, the USB version.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK STANDARD</h3>
+                        <p class="clmn-txt1 text-white">The Standard version supports all basic functionalities of Opal
+                            Lock for up to five USB-mounted Opal Drives. On top of features supported by the Demo
+                            version, the USB version.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="box">
+                        <h3 class="text-white">OPAL LOCK PREMIUM</h3>
+                        <p class="clmn-txt1 text-white">The Standard version supports all basic functionalities of Opal
+                            Lock for up to five USB-mounted Opal Drives. On top of features supported by the Demo
+                            version, the USB version.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section class="xd">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center ">
+                    <img class="img-vec" src="images/Vector (11).png">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec-5">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <table>
+                        <thead class="redbackhead">
+                            <tr class="redback">
+                                <th>Feature</th>
+                                <th>USB</th>
+                                <th>Standard</th>
+                                <th>
+                                    Premium
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="first-row">
+                                <td>Supports up to five USB-mounted Opal Drive(s) on the system</td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td> </td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>Supports internal Opal Drive(s) on the system (five total drives)</td>
+                                <td> </td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                            <tr class="first-row">
+                                <td>Device Query to view additional information</td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td></td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>Set up supported Opal Drive(s)</td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                            <tr class="First-row">
+                                <td>Save hashed password to USB to be used for authentication</td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td></td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>View Audit Log (access a drive’s event log)</td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                            <td>Unlock supported Opal Drive(s)
+                            </td>
+                            <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                    width="5%"></td>
+                            <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                    width="5%"></td>
+                            <td></td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>Unlock with Pre-Boot Authentication
+                                </td>
+                                <td></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                            <td>Create bootable USB drive
+
+                            </td>
+                            <td></td>
+                            <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                    width="5%"></td>
+                            <td></td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>Unlock multiple drives with hashed password securely saved to USB
+
+                                </td>
+                                <td></td>
+                                <td></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                            <td>Set up a second user password with limited authority
+
+                            </td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            </tr>
+                            <tr class="secnd-row">
+                                <td>Automatic Pre-Boot Authentication with password saved on USB
+
+                                </td>
+                                <td></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                                <td><img src="https://wp.project-demo.live/opal-lock/wp-content/uploads/2024/02/check-solid-1.svg"
+                                        width="5%"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+    <?php include 'include/link.php' ?>
+</body>
+
+</html>
