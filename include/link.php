aa8ae3414c91a81ec2209338927a28765f67135f
AhmedRazaDev <maraza008@icloud.com>
2024-03-05 01:55:50 +0500

    Responsive updates

--- include/link.php
+++ include/link.php
@@ -3,3 +3,19 @@
 </script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="../js/app.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script text="jquery">
+$(document).ready(function() {
+    $(".slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: false,
+        prevArrow: '<img width="64" height="64" src="https://img.icons8.com/ios-glyphs/64/6C0808/circled-chevron-left.png" alt="circled-chevron-left"/>',
+        nextArrow: '<img width="64" height="64" src="https://img.icons8.com/ios-glyphs/64/6C0808/circled-chevron-right.png" alt="circled-chevron-right"/>',
+    });
+});
+</script>
